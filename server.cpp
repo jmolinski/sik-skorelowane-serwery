1aec7ad17883daedf1d27a5bcbbfd0775cac8306
Jakub Molinski <jakub@molinski.dev>
2021-04-24 19:38:51 +0200

    Ignore SIGPIPE signal

--- server.cpp
+++ server.cpp
@@ -1,7 +1,10 @@
 #include "filesystem_interactions.hpp"
 #include "tcp_server.hpp"
+#include <signal.h>
 
 int main(int argc, char *argv[]) {
+    signal(SIGPIPE, SIG_IGN);
+
     server(parse_commandline_arguments(argc, argv)).run();
 
     return 0;
